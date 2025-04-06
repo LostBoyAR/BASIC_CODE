13804d3f9304fb3f3de1e5c9b5eebceae6aebe9b
LostBoyAR <adityarajgupta9798@gmail.com>
2025-04-06 20:54:09 +0530

    Add initial implementation of Exp03.java to print welcome message

---
+++ Exp03.java
@@ -0,0 +1,7 @@
+// Experiment 3: Execute the first Program to print “Welcome to LNMCBM in java.
+
+public class Exp03 {
+    public static void main(String ar[]) {
+      System.out.println("Welcome to LNMCBM");
+    }
+  }
